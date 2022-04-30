430f59397393440c47e39fe9467e8d7fc695eeb8
ManFromPig <70714946+Bravci69@users.noreply.github.com>
2022-04-30 13:11:18 +0200

    Update rozvrh 2.D.js

--- commands/rozvrh 2.D.js
+++ commands/rozvrh 2.D.js
@@ -1,1 +1,10 @@
-
+@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'rozvrh 2.D',
+    description: "toto je obrazok rozvrhu",
+    execute(message, args){
+        message.channel.send('Toto je rozvrh: \n'),
+        message.channel.send('https://github.com/DanielGamerLP/rozvrhBOT/blob/main/rozvrhCely.png?raw=true');
+        
+    }
+}
