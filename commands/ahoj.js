66f27a236ba37c49c98a6de94f9cfba18c595d50
DanielGamerLP <93045005+DanielGamerLP@users.noreply.github.com>
2022-05-05 19:33:14 +0200

    rand funkcia na odzdravenie

--- commands/ahoj.js
+++ commands/ahoj.js
@@ -2,6 +2,23 @@ module.exports = {
     name: 'ahoj',
     description: "toto je odzdravenie",
     execute(message, args , Discord){
-        message.channel.send('Nazdar!')
+
+        var rating = Math.floor(Math.random() * 4) + 1;
+
+        if(rating == 1){
+            message.reply('Nazdar!');
+        }
+        if(rating == 2){
+            message.reply('Cau!');
+        }
+        if(rating == 3){
+            message.reply('Cmuk! ;P')
+        }
+        if(rating == 4){
+            message.reply('Ahoj mojko *O*');
+        }
+        
+        message.reply(`Cislo je zo ${rating}/4`);
+
     }
 }
